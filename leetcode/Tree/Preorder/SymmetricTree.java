0fa6c4105f189d3348cf0bf8accdf36e822a7970
Ding Lin <dinglin@dings-mbp.home>
2019-08-16 20:41:36 -0400

    q101

---
+++ leetcode/Tree/Preorder/SymmetricTree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+    return isMirror(root,root);
+}
+private boolean isMirror(TreeNode l, TreeNode r){
+    if(l == null && r == null) return true;
+    if(l == null || r == null) return false;
+
+    return (l.val == r.val) && isMirror(l.left, r.right) && isMirror(l.right, r.left);
+}
+    
+
+}
