ed2dc982fc6508a0b61c936bc1b3162a237ec6b9
Ding Lin <dinglin@Dings-MacBook-Pro.local>
2019-08-20 14:53:39 -0400

    Q80

---
+++ leetcode/Array/RemoveDuplicatesFromSortedArray||.java
@@ -0,0 +1,20 @@
+// time: O(n);
+// space: O(1);
+
+// * @param nums
+// * @return
+// */
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       if (nums.length <= 2) return nums.length;
+        int count = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[count - 2]) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
+
