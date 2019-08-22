29bab821a561c7646adb079265a251ff479ab03e
Ding Lin <dinglin@Dings-MacBook-Pro.local>
2019-08-22 14:51:58 -0400

    q274

---
+++ leetcode/Array/HIndex.java
@@ -0,0 +1,38 @@
+//Time:O(nlogn);
+//space:o(1);
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int i = 0;
+        while (i < citations.length && citations[citations.length - 1 - i] > i) {
+            i++;
+        }
+        return i; 
+    }
+}
+
+//counting sort
+//time:O(n);
+//space:O(n);
+public class Solution {
+    public int hIndex(int[] citations) {
+    int n = citations.length;
+    int[] buckets = new int[n+1];
+    for(int c : citations) {
+        if(c >= n) {
+            buckets[n]++;
+        } else {
+            buckets[c]++;
+        }
+    }
+    int count = 0;
+    for(int i = n; i >= 0; i--) {
+        count += buckets[i];
+        if(count >= i) {
+            return i;
+        }
+    }
+    return 0;
+}
+}
+
