b45f85922378039cd808c6ee478d30829c32d680
Ding Lin <dinglin@Dings-MacBook-Pro.local>
2019-08-28 14:26:54 -0400

    q123

---
+++ leetcode/Array/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,21 @@
+public class BestTimetoBuyandSellStockIII {
+
+    /**
+     * time : O(n)
+     * space : O(1)
+     * @param prices
+     * @return
+     */
+
+    public int maxProfit(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
+        int sell1 = 0, sell2 = 0;
+        for (int price : prices) {
+            sell2 = Math.max(sell2, buy2 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy1 = Math.max(buy1, -price);
+        }
+        return sell2;
+    }
+}
